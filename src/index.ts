1f4174624e90c696fa809696eee9968017814c64
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-03-26 10:22:04 +0530

    initialize project with basic server setup and database schema

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+//import {allRoutes} from "./routes/routes";
+import {serve} from "@hono/node-server";
+import {Hono} from "hono";
+import { secretKey } from "../environment";
+
+const app = new Hono();
+console.log(secretKey)
+app.get("/check", (context) => {
+  return context.json({message: "Hello, World!"}, 200);
+});
+
+serve(app, (info)=>{
+    console.log(`server is running on port ${info.port}`);
+});
