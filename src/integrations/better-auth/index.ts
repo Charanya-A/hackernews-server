9c86cd472e3f1d1938b80d721d6d7849c88d1fba
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-05-09 11:36:06 +0530

    modify

--- src/integrations/better-auth/index.ts
+++ src/integrations/better-auth/index.ts
@@ -6,7 +6,7 @@ import { username } from "better-auth/plugins";
 
 const betterAuthServerClient = betterAuth({
   baseURL: serveUrl,
-  basePath: "/api/auth",
+  basePath: "/auth",
   trustedOrigins: [webClientUrl],
   secret: betterAuthSecret,
   database: prismaAdapter(prismaClient, {
